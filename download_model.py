9901adac0f1da0b29a2b4a998d0c09d2502325c3
fujibo <kt.tsubo@gmail.com>
2018-12-02 22:16:49 +0900

    download model

---
+++ download_model.py
@@ -0,0 +1,4 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+
+gdd.download_file_from_google_drive(file_id='1ykf1q2wyOJufKUNGtgg2-jgcPvFA3NQd', dest_path='./karras2018iclr-lsun-person-256x256.pkl')
